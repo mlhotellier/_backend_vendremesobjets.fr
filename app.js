159a1a309a3aef1382d12f1f4ff9ad82d9523b1e
Mathis L'Hôtellier <mathislhotellier@gmail.com>
2024-05-14 17:28:54 +0200

    Add route for get, add, put and delete things

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/Thing');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-})
+mongoose.connect('mongodb+srv://mlhotellier:oFGYmU2B2BCbLt8d@cluster0.rmjj7sy.mongodb.net/',  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.status(201);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.is })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
 })
 
-app.use((req, res, next) => {
-    res.json({message: "Votre requête a bien été reçue !"});
-    next();
-})
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-})
-
-
-
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }))
+});
 
 module.exports = app;
